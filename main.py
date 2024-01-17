2843c486fc46eb1bc4cbe69cb03e60c4359fc957
kjaurea <kjaurea@gmail.com>
2024-01-17 15:54:44 -0800

    Test master branch changes before merging

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from datetime import *
 if __name__ == '__main__':
     sample_task = Task(title="Closet Case", goaldate=date(2024, 1, 18), goaltime=time(17, 30),
                        description="Clean the dang closet")
-    # sample_task.print_attribute("dtvalue")
 
     sample_task2 = Task("Make pasta", date(2024, 1, 17), time(17, 00), "Make butternut pasta for dinner")
     sample_task3 = Task("Play Grounded", date(2024, 1, 17), time(21, 00), "Kill more stinkbugs")
@@ -20,6 +19,8 @@ if __name__ == '__main__':
     newlist.add_task(sample_task3)
     print("\n")
     newlist.print_priority_list()
+    print("\n")
+    sample_task.print_attribute("description")
 
 # create task manager
 # create a class for tasks, in its own file
