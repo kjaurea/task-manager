8b7a38cc30fab021394d2d746082a0f921624320
kjaurea <kjaurea@gmail.com>
2024-01-17 10:22:14 -0800

    task class testing1

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# This is a sample Python script.
-
+from task import Task
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    sample_task = Task(title="My title", goaldate="Today", goaltime="Noon", description="Clean the dang closet")
+    sample_task.print_task()
 # create task manager
 # create a class for tasks, in its own file
 # create a class for the to-do list, in its own file
--- task.py
+++ task.py
@@ -2,4 +2,12 @@
 class Task:
 
     # constructor
-    def __init__(self, ):
+    def __init__(self, title, goaldate, goaltime, description):
+        self.title = title
+        self.goaldate = goaldate
+        self.goaltime = goaltime
+        self.description = description
+
+    # print method (for testing)
+    def print_task(self):
+       print(f'Task: {self.title}\nDate: {self.goaldate}\nTime: {self.goaltime}\nDescription: {self.description}')
