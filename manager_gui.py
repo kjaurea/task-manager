b2c8366a6018ffe6da82400f8d5051191945b6b8
kjaurea <kjaurea@gmail.com>
2024-01-19 17:28:04 -0800

    finish up gui functionality, working on datepicker

--- manager_gui.py
+++ manager_gui.py
@@ -2,10 +2,13 @@
 # TEST
 import tkinter as tk
 from tkinter import ttk
+from tkinter import scrolledtext
 
 from tktimepicker import AnalogPicker, AnalogThemes
 from tkcalendar import Calendar, DateEntry
 import customtkinter as ctk
+from customtkinter import StringVar
+
 
 class ManagerGUI:
     def __init__(self):
@@ -13,47 +16,62 @@ class ManagerGUI:
 
         # code for window title and dimensions
         self.window.title("Kevin's To-Do List")
-        self.window.geometry("400x600")
+        self.window.geometry("450x350")
 
         # code for application title text and instructions
         self.app_title = ctk.CTkLabel(self.window, text="To-Do List Manager", font=("Calibri", 20))
-        self.app_title.grid(row=0, column=1, columnspan=3)
-        self.app_instructions = ctk.CTkLabel(self.window, text="Enter task details and click submit", font=("Calibri", 14))
-        self.app_instructions.grid(row=1, column=1, columnspan=3)
+        self.app_title.grid(row=0, column=1, columnspan=2)
+        self.app_instructions = ctk.CTkLabel(self.window, text="Enter task details and click submit",
+                                             font=("Calibri", 14))
+        self.app_instructions.grid(row=1, column=1, columnspan=2)
 
-        # code for first input field and corresponding label
+        # code for first input field and corresponding label (Task Name)
         self.task_name_label = ctk.CTkLabel(self.window, text="Task Name")
         self.task_name_label.grid(row=3, column=0, padx=10, pady=10)
-        self.task_name_field = ctk.CTkEntry(self.window, width=250)
+        self.task_name_field = ctk.CTkEntry(self.window, width=250, border_color="black", border_width=2)
         self.task_name_field.grid(row=3, column=1, columnspan=3)
 
-        # code for second input field and corresponding label
+        # code for second input field and corresponding label (Goal Date)
         self.goal_date_label = ctk.CTkLabel(self.window, text="Goal Date")
         self.goal_date_label.grid(row=4, column=0, padx=10, pady=10)
-        self.goal_date_calendar = DateEntry(self.window, width=12, font=("Calibri", 14))
+        # self.goal_date_calendar = DateEntry(self.window, width=12, border_color="black", border_width=2, font=("Calibri", 14))
+        # self.goal_date_calendar.grid(row=4, column=1, columnspan=2)
+        self.goal_date_calendar = ctk.tkCalendar(self.window)
         self.goal_date_calendar.grid(row=4, column=1, columnspan=2)
+        # TODO: fix the date thing...no customTKinter version????
 
-        # code for third input field and corresponding label
+        # code for third input field and corresponding label (Goal Time)
         self.goal_time_label = ctk.CTkLabel(self.window, text="Goal Time")
         self.goal_time_label.grid(row=5, column=0, padx=10, pady=10)
-        self.goal_time_hour = ttk.Combobox(values=[1,2,3,4,5,6,7,8,9,10,11,12], width=5, font=("Calibri", 14))
+        self.goal_time_hour = ctk.CTkComboBox(self.window, values=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+                                        width=65,  font=("Calibri", 14))
         self.goal_time_hour.grid(row=5, column=1)
-        self.goal_time_minute = ttk.Combobox(values=[00, 15, 30, 45], width=6, font=("Calibri", 14))
+        self.goal_time_minute = ctk.CTkComboBox(self.window, values=["00", "15", "30", "45"], width=80, font=("Calibri", 14))
         self.goal_time_minute.grid(row=5, column=2)
-        self.goal_ampm_frame = ctk.CTkFrame(self.window, width=20, height=10)
+        self.goal_ampm_frame = ctk.CTkFrame(self.window)
         self.goal_ampm_frame.grid(row=5, column=3)
-        self.goal_time_am = ctk.CTkRadioButton(self.goal_ampm_frame, text="A.M.", value="am")
-        self.goal_time_am.grid(row=0, column=0)
-        self.goal_time_pm = ctk.CTkRadioButton(self.goal_ampm_frame, text="P.M.", value="pm")
-        self.goal_time_pm.grid(row=1, column=0)
+        self.radio_var = StringVar()
+        self.goal_time_am = ctk.CTkRadioButton(self.goal_ampm_frame, variable=self.radio_var, radiobutton_width=15,
+                                               radiobutton_height=15,
+                                               border_width_unchecked=3, border_width_checked=4, text="A.M.",
+                                               value="am", font=("Calibri", 12))
+        self.goal_time_am.pack(pady=(2, 0), padx=4)
+        self.goal_time_pm = ctk.CTkRadioButton(self.goal_ampm_frame, variable=self.radio_var, radiobutton_width=15,
+                                               radiobutton_height=15,
+                                               border_width_unchecked=3, border_width_checked=4, text="P.M.",
+                                               value="pm", font=("Calibri", 12))
+        self.goal_time_pm.pack(pady=(0, 2), padx=4)
 
-
-        # code for fourth input field and corresponding label
-
+        # code for fourth input field and corresponding label (Description)
+        self.task_desc_label = ctk.CTkLabel(self.window, text="Task Description")
+        self.task_desc_label.grid(row=6, column=0, padx=10, pady=10)
+        self.task_desc_field = ctk.CTkTextbox(self.window, width=300, height=80, wrap="word", border_color="black",
+                                              border_width=2, font=("Calibri", 12))
+        self.task_desc_field.grid(row=6, column=1, columnspan=3, pady=(20, 0))
 
         # code for submission button
         self.submit_button = ctk.CTkButton(self.window, text="Add to list", command=self.submit_button_callback)
-        self.submit_button.grid(row=6, column=1, columnspan=3)
+        self.submit_button.grid(row=7, column=1, columnspan=2, pady=10)
 
         # mainloop() is needed at end to show actual window. Code above mainloop is executed before opening window
         self.window.mainloop()
