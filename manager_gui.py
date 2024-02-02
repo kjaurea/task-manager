ca92d4878a7d00b1dd3330035d593f6ea7da093a
kjaurea <kjaurea@gmail.com>
2024-02-02 11:46:40 -0800

    work on date and time inputs

--- manager_gui.py
+++ manager_gui.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 
-from tktimepicker import AnalogPicker, AnalogThemes
+from datetime import *
 from tkcalendar import Calendar, DateEntry
 import customtkinter as ctk
 from customtkinter import StringVar
@@ -34,19 +34,21 @@ class ManagerGUI:
         # code for second input field and corresponding label (Goal Date)
         self.goal_date_label = ctk.CTkLabel(self.window, text="Goal Date")
         self.goal_date_label.grid(row=4, column=0, padx=10, pady=10)
-        # self.goal_date_calendar = DateEntry(self.window, width=12, border_color="black", border_width=2, font=("Calibri", 14))
-        # self.goal_date_calendar.grid(row=4, column=1, columnspan=2)
-        self.goal_date_calendar = ctk.tkCalendar(self.window)
+        self.goal_date_calendar = DateEntry(self.window, width=12, border_color="black", border_width=2,
+                                            font=("Calibri", 14))
+        self.goal_date_calendar.grid(row=4, column=1, columnspan=2)
         self.goal_date_calendar.grid(row=4, column=1, columnspan=2)
         # TODO: fix the date thing...no customTKinter version????
 
         # code for third input field and corresponding label (Goal Time)
         self.goal_time_label = ctk.CTkLabel(self.window, text="Goal Time")
         self.goal_time_label.grid(row=5, column=0, padx=10, pady=10)
-        self.goal_time_hour = ctk.CTkComboBox(self.window, values=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-                                        width=65,  font=("Calibri", 14))
+        self.goal_time_hour = ctk.CTkComboBox(self.window,
+                                              values=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+                                              width=65, font=("Calibri", 14))
         self.goal_time_hour.grid(row=5, column=1)
-        self.goal_time_minute = ctk.CTkComboBox(self.window, values=["00", "15", "30", "45"], width=80, font=("Calibri", 14))
+        self.goal_time_minute = ctk.CTkComboBox(self.window, values=["00", "15", "30", "45"], width=80,
+                                                font=("Calibri", 14))
         self.goal_time_minute.grid(row=5, column=2)
         self.goal_ampm_frame = ctk.CTkFrame(self.window)
         self.goal_ampm_frame.grid(row=5, column=3)
@@ -61,6 +63,7 @@ class ManagerGUI:
                                                border_width_unchecked=3, border_width_checked=4, text="P.M.",
                                                value="pm", font=("Calibri", 12))
         self.goal_time_pm.pack(pady=(0, 2), padx=4)
+        self.radio_var.set("am") #TODO: need to set am as default? It keeps thinking PM selected....
 
         # code for fourth input field and corresponding label (Description)
         self.task_desc_label = ctk.CTkLabel(self.window, text="Task Description")
@@ -70,14 +73,30 @@ class ManagerGUI:
         self.task_desc_field.grid(row=6, column=1, columnspan=3, pady=(20, 0))
 
         # code for submission button
-        self.submit_button = ctk.CTkButton(self.window, text="Add to list", command=self.submit_button_callback)
+        self.submit_button = ctk.CTkButton(self.window, text="Add Task", command=self.submit_button_callback)
         self.submit_button.grid(row=7, column=1, columnspan=2, pady=10)
 
         # mainloop() is needed at end to show actual window. Code above mainloop is executed before opening window
         self.window.mainloop()
 
     def submit_button_callback(self):
-        print("Task submitted")
+        new_task_name = self.task_name_field.get()
+        new_task_date = self.goal_date_calendar.get()
+        # new_task_date = datetime.strptime(self.goal_date_calendar.get(), '%m/%d/%Y').date() #TODO: doesn't work. Datepicker already returns date type???
+        new_task_desc = self.task_desc_field.get(1.0,
+                                                 "end-1c")  # TODO: finish converting datetime date and datetime time...
+        new_task_hour = int(self.goal_time_hour.get())
+        if self.goal_time_pm:
+            print("PM selected")
+            new_task_hour += 12
+            print(new_task_hour)
+        new_task_minute = int(self.goal_time_minute.get())
+        new_task_time = time(hour=new_task_hour, minute=new_task_minute, second=0)
+
+        print(new_task_name)
+        print(new_task_date)
+        print(new_task_desc)
+        print(new_task_time)
 
     # def __init__(self):
     #     self.root = ctk.CTk()
